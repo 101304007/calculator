f82dfabb6edf7d11b112e13f8e9c6ecd25f3e220
許仲廷 <101304007@nccu.edu.tw>
2015-03-29 23:48:13 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<link type="text/css" rel="stylesheet" href="app.css">
+	<meta charset="UTF-8">
+    <title>caculator</title>
+	<h2 >?My calculator</h2>
+
+    <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="app.js"></script>
+
+	<style type=text/css>
+<!--
+body{
+background-image:url(https://www.google.com.tw/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0CAcQjRw&url=http%3A%2F%2Fwww.syhobby.com%2Fproducts_show.php%3Fsn%3D12943&ei=FhkYVaKyPMSm8AWghYBg&psig=AFQjCNEH7iAD5hJSiS6dRPbbHpagVlnpag&ust=1427729045319293);
+background-position:0% 50%;
+background-attachment:fixed;
+background-repeat:no-repeat;}
+-->
+</style>
+</head>
+
+<body>
+<div td align="left" >
+    <div class='large'>
+        <p id="message">0</p>
+        <p id="operator">@</p>
+    </div>
